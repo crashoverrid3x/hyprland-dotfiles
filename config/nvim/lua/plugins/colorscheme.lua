bdb943291f3b34e998e352863fc25a96c3b7f37c
Juan Alberto <crashoverride@hacker.hack4u>
2026-02-27 02:41:43 +0100

    neovim update

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,24 @@
 return {
   {
-    "folke/tokyonight.nvim",
-    opts = {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    },
+    "olimorris/onedarkpro.nvim",
+    priority = 1000, -- que cargue primero
+    lazy = false, -- evita carga perezosa porque es tu theme principal
+    config = function()
+      require("onedarkpro").setup({
+        options = {
+          transparency = false, -- haz true si quieres fondo transparente
+          terminal_colors = true,
+          highlight_inactive_windows = false,
+        },
+        colors = {},
+        highlights = {},
+        plugins = {
+          treesitter = true,
+          nvim_lsp = true,
+        },
+      })
+      -- establece OneDark Pro como tema
+      vim.cmd("colorscheme onedark")
+    end,
   },
 }
