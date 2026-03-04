9bf4b776bf4ba765d61e07ff797f90f1a7d848d5
v3ctorsh4de <181494272+v3ctorsh4de@users.noreply.github.com>
2026-03-04 18:30:00 +0000

    Update hyprland_autoinstall.sh

--- hyprland_autoinstall.sh
+++ hyprland_autoinstall.sh
@@ -6,7 +6,7 @@ git clone https://github.com/v3ctorsh4de/hyprland-dotfiles.git
 
 echo -e "\n\nInstalamos herramientas necesarias\n\n"
 
-sudo pacman -S paru yay hyprland waybar kitty git curl wget zsh
+sudo pacman -S paru yay hyprland waybar kitty git curl wget zsh hyprpaper rofi python3 golang cmake make ruby perl
 
 echo -e "\n\n\nAhora vamos a agregar la configuracion a los archivos correspondientes\n\n"
 
@@ -17,5 +17,8 @@ cp -r hyprland-dotfiles/wallpapers
 echo -e "\n\n Hora de configurar zsh y agregar la configuracion:\n\n"
 
 cp hyprland-dotfiles/.zshrc .
+
+echo -e "Configurando el fondo de pantalla: "
+sudo pacman -S swww
 
 echo -e "\n\nRecuerda editar la configuracion que hay en /home/usuario/.config/hypr/hyprland.conf para cambiar las resoluciones y el escalado de su pantalla para una mejor experiencia.\n"
